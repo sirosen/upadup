e42e8b49fa2d14e6bb17bc1bebeb27bab3e63011
Kurt McKee <contactme@kurtmckee.org>
2024-06-04 08:43:45 -0500

    Add a timeout to PyPI JSON requests

--- src/upadup/package_utils.py
+++ src/upadup/package_utils.py
@@ -7,6 +7,8 @@ def normalize_package_name(name: str):
 
 
 def get_pkg_latest(name: str) -> str:
-    with urllib.request.urlopen(f"https://pypi.python.org/pypi/{name}/json") as conn:
+    with urllib.request.urlopen(
+        f"https://pypi.python.org/pypi/{name}/json", timeout=30
+    ) as conn:
         version_data = json.load(conn)
     return str(version_data["info"]["version"])
