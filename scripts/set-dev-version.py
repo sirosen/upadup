5ee27640ba6eaf51fdf759cfff59ccb93e56139e
Stephen Rosen <sirosen@globus.org>
2025-02-10 16:12:04 -0600

    Update project configs to allow CI test & publish
    Setup workflows (based on prior projects) for CI testing and PyPI
    publishing.

---
+++ scripts/set-dev-version.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import argparse
+import re
+
+
+def get_old_version():
+    with open("pyproject.toml", encoding="utf-8") as fp:
+        content = fp.read()
+    match = re.search(r'^version = "(\d+\.\d+\.\d+)"$', content, flags=re.MULTILINE)
+    assert match
+    return match.group(1)
+
+
+def replace_version(filename, formatstr, old_version, new_version):
+    print(f"updating {filename}")
+    with open(filename, encoding="utf-8") as fp:
+        content = fp.read()
+    old_str = formatstr.format(old_version)
+    new_str = formatstr.format(new_version)
+    content = content.replace(old_str, new_str)
+    with open(filename, "w", encoding="utf-8") as fp:
+        fp.write(content)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--number", help="dev number to use, defaults to 1", type=int, default=1
+    )
+    args = parser.parse_args()
+
+    old_version = get_old_version()
+    new_version = old_version + f".dev{args.number}"
+
+    replace_version("pyproject.toml", 'version = "{}"', old_version, new_version)
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
